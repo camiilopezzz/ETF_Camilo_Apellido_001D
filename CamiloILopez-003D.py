a842bb834e58d39869f112124916976ff5e8f34a
camilolopezzz <VA-Alumno@duoc.acad>
2026-07-10 13:29:44 -0400

    Creacion de validaciones y opciones del menu

--- CamiloILopez-003D.py
+++ CamiloILopez-003D.py
@@ -54,3 +54,42 @@ def validar_cupos(cupos):
         return True
     return False
 
+def agregar_codigo(lista_codigo):
+    codigo_plan = input("Ingrese su codigo: ")
+    if not validar_texto(codigo_plan):
+        return
+    print("Error: no puede estar vacio o contener espacios. ")
+    
+    nombre_plan = input("Ingrese nombre del plan: ")
+    if not validar_texto(nombre_plan):
+        print("Error: no debe estar vacio ni contener espacios.")
+        return
+    
+    tipo_plan = input("Ingrese tipo (mensual/trimestral/anual: )")
+    if not validar_plan(tipo_plan):
+        print("Error escriba uno de los seleccionados en pantalla.")
+        return
+    
+    duracion_meses = int("Ingrese la duracion: ")
+    if not duracion(duracion_meses):
+        print("Error: ingrese numero entero.")
+        return
+    
+
+    try:
+        precio = int(input("Ingrese el precio: "))
+    except ValueError:
+        print("Error: debe ser un numero entero.")
+    else:
+        if not validar_precio(precio):
+            return
+        plan_mensual = int
+
+    nuevocupo = {
+        "codigo": codigo_plan,
+        "nombre": nombre_plan,
+        "duracion": duracion_meses,
+        "precio": precio,
+    }
+    lista_codigo.append(nuevocupo)
+    print(f"Registrado con exito {nuevocupo}")
